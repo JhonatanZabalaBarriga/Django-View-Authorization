10439d10157b65d2c22ef89010f6f5b66cc0b5d0
JhonatanZabalaBarriga <zabalaba12@gmail.com>
2022-05-27 01:17:50 -0500

    messaging a logged-in user

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from core.models import Blog
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
 
 def listing(request):
     data = {
@@ -53,3 +54,11 @@ def private_place(request):
 @user_passes_test(lambda user: user.is_staff)
 def staff_place(request):
     return HttpResponse("Employees must wash hands", content_type="text/plain")
+
+@login_required
+def add_messages(request):
+    username = request.user.username
+    messages.add_message(request, messages.INFO, f"Hello {username}")
+    messages.add_message(request, messages.WARNING, "DANGER WILL ROBINSON")
+
+    return HttpResponse("Messages added", content_type="text/plain")
